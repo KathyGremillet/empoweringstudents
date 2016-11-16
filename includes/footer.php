df06f48e87726cf04bbc8155859159c991f617b0
Kathy <gremillet.kathy@gmail.com>
2016-11-16 21:22:18 +0100

    modifications liens javascript

--- includes/footer.php
+++ includes/footer.php
@@ -5,7 +5,7 @@
 		</div>		
 	</footer>
 
-	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.min.js"></script>
+	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.min.js"></script>
 	<script src="./js/production.min.js" type="text/javascript"></script>
 	
 </body>
--- includes/header.php
+++ includes/header.php
@@ -10,8 +10,6 @@
 	<meta name="description" xml:lang="fr" content="<?php echo $description ?>" />
 
 	<link rel="stylesheet" type="text/css" href="./css/global.css" />
-	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.min.js"></script>
-	<script src="./js/production.min.js" type="text/javascript"></script>
 
 	<meta content="width=device-width, initial-scale=1, maximum-scale=1" name="viewport">
 
